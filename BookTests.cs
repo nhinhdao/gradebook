7632500bd5732e741b4657a2448dc89fb7c143b8
Nhinh Dao <nhinhdao.vn@gmail.com>
2019-06-01 01:50:45 -0400

    Added Xunit test

---
+++ BookTests.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+
+namespace GradeBook.tests {
+    public class BookTests {
+        [Fact]
+        public void Test1 () {
+            // arrange
+            var book = new Book ("Nhinh");
+            book.AddGrade (89.3);
+            book.AddGrade (99.3);
+            book.AddGrade (70.3);
+
+            // act
+            var result = book.GetStatistics ();
+
+            // assert
+            Assert.Equal (86.3, result.Average, 1);
+            Assert.Equal (99.3, result.HighestGrade, 1);
+            Assert.Equal (70.3, result.LowestGrade, 1);
+
+            Console.WriteLine ($"Highest grade: {result.HighestGrade}");
+            Console.WriteLine ($"Lowest grade: {result.LowestGrade}");
+            Console.WriteLine ($"Average grade: {result.Average}");
+        }
+    }
+}
